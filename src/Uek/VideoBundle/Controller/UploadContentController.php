46fbc35f1f527c475e6d51e9565efa19f173fb87
macko <macko@localhost>
2015-02-12 00:14:08 +0100

    Content loader

---
+++ src/Uek/VideoBundle/Controller/UploadContentController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Uek\VideoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Uek\VideoBundle\Entity\Movie;
+use Symfony\Component\HttpFoundation\Response;
+
+class UploadContentController extends Controller
+{
+    public function movie1Action()
+    {
+    	$movie = new Movie();
+    	$movie->setGenre('Komedia');
+    	$movie->setPicture('forrestGumpS.jpg');
+    	$movie->setTitle('Forrest Gump');
+    	$movie->setPrice(5);
+    	
+    	$em = $this->getDoctrine()->getManager();
+    	$em->persist($movie);
+    	$em->flush();
+    	
+    	return new Response('Created product id '.$movie->getId());
+    }
+
+	public function movie2Action()
+	{
+		$movie = new Movie();
+		$movie->setGenre('Komedia');
+		$movie->setPicture('itouchablesS.jpg');
+		$movie->setTitle('Nietykalni');
+		$movie->setPrice(5);
+		 
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($movie);
+		$em->flush();
+		 
+		return new Response('Created product id '.$movie->getId());
+	}
+	
+	public function movie3Action()
+	{
+		$movie = new Movie();
+		$movie->setGenre('Gangsterski');
+		$movie->setPicture('pulpFictionS.jpg');
+		$movie->setTitle('Pulp Fiction');
+		$movie->setPrice(7);
+			
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($movie);
+		$em->flush();
+			
+		return new Response('Created product id '.$movie->getId());
+	}
+	
+	public function movie4Action()
+	{
+		$movie = new Movie();
+		$movie->setGenre('Anime');
+		$movie->setPicture('spiritedAwayS.jpg');
+		$movie->setTitle('W krainie Bogów');
+		$movie->setPrice(6);
+			
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($movie);
+		$em->flush();
+			
+		return new Response('Created product id '.$movie->getId());
+	}
+	
+	public function movie5Action()
+	{
+		$movie = new Movie();
+		$movie->setGenre('Gangsterski');
+		$movie->setPicture('theGodfatherS.jpg');
+		$movie->setTitle('Ociec Chrzestny');
+		$movie->setPrice(6);
+			
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($movie);
+		$em->flush();
+			
+		return new Response('Created product id '.$movie->getId());
+	}
+	
+	public function movie6Action()
+	{
+		$movie = new Movie();
+		$movie->setGenre('Dokumentalny');
+		$movie->setPicture('theInternetsOwnBoyS.jpg');
+		$movie->setTitle('The internet Own Boy');
+		$movie->setPrice(6);
+			
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($movie);
+		$em->flush();
+			
+		return new Response('Created product id '.$movie->getId());
+	}
+	
+	public function movie7Action()
+	{
+		$movie = new Movie();
+		$movie->setGenre('Dramat');
+		$movie->setPicture('theShawshankRedemption.jpg');
+		$movie->setTitle('Skazani na Shawshank');
+		$movie->setPrice(5);
+			
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($movie);
+		$em->flush();
+			
+		return new Response('Created product id '.$movie->getId());
+	}
+}
